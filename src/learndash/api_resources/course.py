968d3a9a797775110469f554001180224865a0da
Jorgen <jorgen@buildnode.io>
2021-04-20 22:44:50 -0700

    add api resources

--- src/learndash/api_resources/course.py
+++ src/learndash/api_resources/course.py
@@ -4,21 +4,45 @@ from learndash.api_resources.abstract import RetrievableAPIResource
 from learndash.api_resources.abstract import UpdateableAPIResource
 from learndash.api_resources.abstract import NestedAPIResource
 
-class Course(RetrievableAPIResource, ListableAPIResource):
-    """
-    """
-
+class Course(RetrievableAPIResource, ListableAPIResource): # Also deletable, creatable, updateable
     api_path = learndash.path_courses
+    
+    def steps(self, id=None):
+        return CourseSteps(id, parent=self)
+    
+    def prerequisites(self, id=None):
+        return CoursePrerequisites(id, parent=self)
 
     def users(self, id=None):
         return CourseUser(id, parent=self)
+    
+    def groups(self, id=None):
+        return CourseGroups(id, parent=self)
+
+
+class CourseSteps(ListableAPIResource, UpdateableAPIResource, NestedAPIResource):
+    api_path = learndash.path_course_steps
+
+    def instance_url(self):
+            # This endpoint accepts updates at it's base endpoint
+            return self.class_url()
+
+
+class CoursePrerequisites(ListableAPIResource, NestedAPIResource):
+    api_path = learndash.path_course_prerequisites
 
 
 class CourseUser(ListableAPIResource, UpdateableAPIResource, NestedAPIResource):
-        """
-        """
-        api_path = learndash.path_course_users
+    api_path = learndash.path_course_users
 
-        def instance_url(self):
-            # This endpoint accepts updates and deletions at it's base endpoint
-            return self.class_url()
+    def instance_url(self):
+        # This endpoint accepts updates and deletions at it's base endpoint
+        return self.class_url()
+
+
+class CourseGroups(ListableAPIResource, UpdateableAPIResource, NestedAPIResource): # Also deletable
+    api_path = learndash.path_course_groups
+
+    def instance_url(self):
+        # This endpoint accepts updates and deletions at it's base endpoint
+        return self.class_url()
--- src/learndash/api_resources/user.py
+++ src/learndash/api_resources/user.py
@@ -5,16 +5,44 @@ from learndash.api_resources.abstract import UpdateableAPIResource
 from learndash.api_resources.abstract import NestedAPIResource
 
 class User(RetrievableAPIResource, ListableAPIResource):
-    """
-    """
 
     api_path = learndash.path_users
 
     def course_progress(self, id=None):
         return UserCourseProgress(id, parent=self)
 
+    def courses(self, id=None):
+        return UserCourse(id, parent=self)
+    
+    def groups(self, id=None):
+        return UserGroup(id, parent=self)
+
+    def quiz_progress(self, id=None):
+        return UserQuizProgress(id, parent=self)
+
 
 class UserCourseProgress(ListableAPIResource, NestedAPIResource):
-        """
-        """
-        api_path = learndash.path_user_course_progress
+    api_path = learndash.path_user_course_progress
+
+
+# class UserCourseProgressSteps(ListableAPIResource, NestedAPIResource):
+    
+
+class UserCourse(ListableAPIResource, UpdateableAPIResource, NestedAPIResource): # also deletable
+    api_path = learndash.path_user_courses
+
+    def instance_url(self):
+        # This endpoint accepts updates and deletions at it's base endpoint
+        return self.class_url()
+
+
+class UserGroup(ListableAPIResource, UpdateableAPIResource, NestedAPIResource): # also deleteable
+    api_path = learndash.path_user_groups
+
+    def instance_url(self):
+        # This endpoint accepts updates and deletions at it's base endpoint
+        return self.class_url()
+
+
+class UserQuizProgress(ListableAPIResource, NestedAPIResource):
+    api_path = learndash.path_user_quiz_progress
