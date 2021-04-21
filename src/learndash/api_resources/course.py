6455c98e1e1999bb8a327872941846bc231adcd8
Jorgen <jorgen@buildnode.io>
2021-04-20 21:50:14 -0700

    add course users resource

--- src/learndash/api_resources/course.py
+++ src/learndash/api_resources/course.py
@@ -1,8 +1,24 @@
 import learndash
 from learndash.api_resources.abstract import ListableAPIResource
 from learndash.api_resources.abstract import RetrievableAPIResource
+from learndash.api_resources.abstract import UpdateableAPIResource
+from learndash.api_resources.abstract import NestedAPIResource
 
 class Course(RetrievableAPIResource, ListableAPIResource):
     """
     """
+
     api_path = learndash.path_courses
+
+    def users(self, id=None):
+        return CourseUser(id, parent=self)
+
+
+class CourseUser(ListableAPIResource, UpdateableAPIResource, NestedAPIResource):
+        """
+        """
+        api_path = learndash.path_course_users
+
+        def instance_url(self):
+            # This endpoint accepts updates and deletions at it's base endpoint
+            return self.class_url()
