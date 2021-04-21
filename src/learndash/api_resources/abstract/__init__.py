41a1eaa17bd5af772a39b01e1304120b092236ca
Jorgen <jorgen@buildnode.io>
2021-04-20 21:49:23 -0700

    add abstract imports

--- src/learndash/api_resources/abstract/__init__.py
+++ src/learndash/api_resources/abstract/__init__.py
@@ -1,3 +1,5 @@
 from learndash.api_resources.abstract.api_resource import APIResource
+from learndash.api_resources.abstract.nested_api_resource import NestedAPIResource
 from learndash.api_resources.abstract.retrievable_api_resource import RetrievableAPIResource
 from learndash.api_resources.abstract.listable_api_resource import ListableAPIResource
+from learndash.api_resources.abstract.updateable_api_resource import UpdateableAPIResource
