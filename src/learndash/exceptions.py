01d04343f78d26288ea3e7bc17ba3232c3eb18b5
Jorgen <jorgen@buildnode.io>
2021-05-05 15:19:27 -0700

    fix exception class

--- src/learndash/exceptions.py
+++ src/learndash/exceptions.py
@@ -1,6 +1,8 @@
+import logging
+
 class LearndashException(Exception):
 
-    def __init__(self, message):
+    def __init__(self, message=None):
         super().__init__(message)
         if message:
             self.message = message
